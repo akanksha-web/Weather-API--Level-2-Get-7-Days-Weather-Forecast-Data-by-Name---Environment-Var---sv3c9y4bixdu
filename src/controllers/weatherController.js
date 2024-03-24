4cc4555857500ddcd516b53812dc5aabba77dcde
Akanksha Dubey <67544813+akanksha-web@users.noreply.github.com>
2024-03-24 22:50:21 +0530

    Update weatherController.js

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -41,7 +41,41 @@ async function saveDataToDatabase(data) {
 // Level 2: Get 7 Days Weather Forecast Data by Name
 async function getForecastDataByName(cityName) {
   
+async function getForecastDataByName(cityName) {
+  
+  try{
+    const data = await getDataFromDatabase();
+    const city = data.find(city => city.name.toLowerCase() === cityName.toLowerCase());
 
+     if(city){
+      return{
+        statusCode: 200,
+        Response:{
+          status :"success",
+          message: "Forecast data retrieved",
+          data: city.forecast
+        }
+      };
+     }else{
+      return{
+        statusCode: 404,
+        Response:{
+          status:"error",
+          message:"Failed to retrieve forecast data",
+          error:"City not found"
+        }
+      };
+     }
+  }catch(error){
+    return{
+      statusCode:500,
+      Response:{
+        status:"error",
+        message:"Internal server error",
+        error: error.message
+      }
+    };
+  }
   // TODO: Implement this function
   
 }
